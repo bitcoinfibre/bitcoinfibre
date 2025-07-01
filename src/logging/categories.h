e76e4819c79219ac9b3c853829c632c4d5d8302d
w0xlt <94266259+w0xlt@users.noreply.github.com>
2025-07-01 14:50:04 -0700

    Rebase Fibre project

--- src/logging/categories.h
+++ src/logging/categories.h
@@ -46,6 +46,8 @@ enum LogFlags : CategoryMask {
     TXPACKAGES = (CategoryMask{1} << 28),
     KERNEL = (CategoryMask{1} << 29),
     PRIVBROADCAST = (CategoryMask{1} << 30),
+    UDPNET = (CategoryMask{1} << 31),
+    FEC = (CategoryMask{1} << 32),
     ALL = ~NONE,
 };
 
