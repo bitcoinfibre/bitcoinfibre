w0xlt/bitcoin

Make the project compatible with MacOS AppleSilicon (ARM)

src/udpnet.cpp
@@ -80,7 +80,7 @@ static node::NodeContext* g_node_context; // Initialized by InitializeUDPConnect
 static CService LOCAL_WRITE_DEVICE_SERVICE(CNetAddr(), 1);
 static CService LOCAL_READ_DEVICE_SERVICE(CNetAddr(), 2);
 
-#define LOCAL_DEVICE_CHECKSUM_MAGIC htole64(0xdeadbeef)
+#define LOCAL_DEVICE_CHECKSUM_MAGIC htole64_internal(0xdeadbeef)
 
 // TODO: The checksum stuff is not endian-safe (esp the poly impl):
 static void FillChecksum(uint64_t magic, UDPMessage& msg, const unsigned int length)
@@ -228,27 +228,49 @@ bool InitializeUDPConnections(node::NodeContext* const node_context) {
 
     const std::vector<std::pair<unsigned short, uint64_t> > group_list(GetUDPInboundPorts());
     for (std::pair<unsigned short, uint64_t> port : group_list) {
-        udp_socks.push_back(socket(AF_INET6, SOCK_DGRAM, 0));
-        assert(udp_socks.back());
-
-        int opt = 1;
-        assert(setsockopt(udp_socks.back(), SOL_SOCKET, SO_REUSEADDR, &opt,  sizeof(opt)) == 0);
-        opt = 0;
-        assert(setsockopt(udp_socks.back(), IPPROTO_IPV6, IPV6_V6ONLY, &opt,  sizeof(opt)) == 0);
-        fcntl(udp_socks.back(), F_SETFL, fcntl(udp_socks.back(), F_GETFL) | O_NONBLOCK);
-
-        struct sockaddr_in6 wildcard;
-        memset(&wildcard, 0, sizeof(wildcard));
-        wildcard.sin6_family = AF_INET6;
-        memcpy(&wildcard.sin6_addr, &in6addr_any, sizeof(in6addr_any));
-        wildcard.sin6_port = htons(port.first);
-
-        if (bind(udp_socks.back(), (sockaddr*) &wildcard, sizeof(wildcard))) {
+        // Create separate IPv6 and IPv4 sockets for this UDP port
+        int sock6 = socket(AF_INET6, SOCK_DGRAM, 0);
+        if (sock6 < 0) {
             CloseSocketsAndReadEvents();
             return false;
         }
-
-        LogPrintf("UDP: Bound to port %hd for group %lu with %lu Mbps\n", port.first, udp_socks.size() - 1, port.second);
+        int opt = 1;
+        assert(setsockopt(sock6, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == 0);
+        assert(setsockopt(sock6, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof(opt)) == 0);
+        fcntl(sock6, F_SETFL, fcntl(sock6, F_GETFL) | O_NONBLOCK);
+        struct sockaddr_in6 addr6;
+        memset(&addr6, 0, sizeof(addr6));
+        addr6.sin6_family = AF_INET6;
+        addr6.sin6_addr = in6addr_any;
+        addr6.sin6_port = htons(port.first);
+        if (bind(sock6, (sockaddr*)&addr6, sizeof(addr6)) != 0) {
+            CloseSocketsAndReadEvents();
+            close(sock6);
+            return false;
+        }
+        int sock4 = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sock4 < 0) {
+            CloseSocketsAndReadEvents();
+            close(sock6);
+            return false;
+        }
+        assert(setsockopt(sock4, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == 0);
+        fcntl(sock4, F_SETFL, fcntl(sock4, F_GETFL) | O_NONBLOCK);
+        struct sockaddr_in addr4;
+        memset(&addr4, 0, sizeof(addr4));
+        addr4.sin_family = AF_INET;
+        addr4.sin_addr.s_addr = INADDR_ANY;
+        addr4.sin_port = htons(port.first);
+        if (bind(sock4, (sockaddr*)&addr4, sizeof(addr4)) != 0) {
+            CloseSocketsAndReadEvents();
+            close(sock6);
+            close(sock4);
+            return false;
+        }
+        // Register both sockets and log the binding
+        udp_socks.push_back(sock6);
+        udp_socks.push_back(sock4);\
+        LogPrintf("UDP: Bound to port %hu for group %lu with %lu Mbps\n", port.first, (unsigned long)(udp_socks.size()/2 - 1), port.second);
     }
 
     event_base_read = event_base_new();
@@ -381,25 +403,29 @@ static void read_socket_func(evutil_socket_t fd, short event, void* arg) {
     std::chrono::steady_clock::time_point start(std::chrono::steady_clock::now());
 
     UDPMessage msg;
-    /* We will place the incoming UDP message payload into `msg`. However, not
-     * necessarily the incoming payload will fill the entire `UDPMessage`
-     * structure. Hence, zero-initialize `msg` here. */
-    struct sockaddr_in6 remoteaddr;
-    socklen_t remoteaddrlen = sizeof(remoteaddr);
-
-    ssize_t res = recvfrom(fd, &msg, sizeof(msg), MSG_DONTWAIT, (sockaddr*)&remoteaddr, &remoteaddrlen);
+    sockaddr_storage remote_addr;
+    socklen_t remote_addr_len = sizeof(remote_addr);
+    ssize_t res = recvfrom(fd, &msg, sizeof(msg), MSG_DONTWAIT, (sockaddr*)&remote_addr, &remote_addr_len);
+     
     if (res < 0) {
         int err = errno;
         LogPrintf("UDP: Error reading from socket: %d (%s)!\n", err, NetworkErrorString(err));
         return;
     }
-    assert(remoteaddrlen == sizeof(remoteaddr));
 
     if (size_t(res) < sizeof(UDPMessageHeader) || size_t(res) >= sizeof(UDPMessage))
         return;
 
+    CService remote_service;
+    if (((sockaddr*)&remote_addr)->sa_family == AF_INET6) {
+        remote_service = CService(*(sockaddr_in6*)&remote_addr);
+    } else if (((sockaddr*)&remote_addr)->sa_family == AF_INET) {
+        remote_service = CService(*(sockaddr_in*)&remote_addr);
+    } else {
+        return;
+    }
     std::unique_lock<std::recursive_mutex> lock(cs_mapUDPNodes);
-    std::map<CService, UDPConnectionState>::iterator it = mapUDPNodes.find(CService(remoteaddr));
+    auto it = mapUDPNodes.find(remote_service);
     if (it == mapUDPNodes.end())
         return;
     if (!CheckChecksum(it->second.connection.local_magic, msg, res))
@@ -417,7 +443,7 @@ static void read_socket_func(evutil_socket_t fd, short event, void* arg) {
             return;
         }
 
-        state.protocolVersion = le64toh(msg.msg.longint);
+        state.protocolVersion = le64toh_internal(msg.msg.longint);
         if (PROTOCOL_VERSION_MIN(state.protocolVersion) > PROTOCOL_VERSION_CUR(UDP_PROTOCOL_VERSION)) {
             LogPrintf("UDP: Got min protocol version we didnt understand (%u:%u) from %s\n", PROTOCOL_VERSION_MIN(state.protocolVersion), PROTOCOL_VERSION_CUR(state.protocolVersion), it->first.ToStringAddrPort());
             send_and_disconnect(it);
@@ -472,7 +498,7 @@ static void read_socket_func(evutil_socket_t fd, short event, void* arg) {
             return;
         }
 
-        uint64_t nonce = le64toh(msg.msg.longint);
+        uint64_t nonce = le64toh_internal(msg.msg.longint);
         std::map<uint64_t, int64_t>::iterator nonceit = state.ping_times.find(nonce);
         if (nonceit == state.ping_times.end()) // Possibly duplicated packet
             LogPrintf("UDP: Got PONG message without PING from %s\n", it->first.ToStringAddrPort());
@@ -635,7 +661,7 @@ static void timer_func(evutil_socket_t fd, short event, void* arg) {
 
         if (!(state.state & STATE_GOT_SYN_ACK) && origLastSendTime < now - 1000) {
             msg.header.msg_type = MSG_TYPE_SYN;
-            msg.msg.longint = htole64(UDP_PROTOCOL_VERSION);
+            msg.msg.longint = htole64_internal(UDP_PROTOCOL_VERSION);
             SendMessage(msg, sizeof(UDPMessageHeader) + 8, false, it);
             state.lastSendTime = now;
         }
@@ -649,7 +675,7 @@ static void timer_func(evutil_socket_t fd, short event, void* arg) {
         if ((state.state & STATE_INIT_COMPLETE) == STATE_INIT_COMPLETE && state.lastPingTime < now - 1000 * 60 * 15) {
             uint64_t pingnonce = FastRandomContext().rand64();
             msg.header.msg_type = MSG_TYPE_PING;
-            msg.msg.longint = htole64(pingnonce);
+            msg.msg.longint = htole64_internal(pingnonce);
             SendMessage(msg, sizeof(UDPMessageHeader) + 8, false, it);
             state.ping_times[pingnonce] = TicksSinceEpoch<std::chrono::microseconds>(SystemClock::now());
             state.lastPingTime = now;
@@ -776,7 +802,8 @@ static inline bool fill_cache(PerQueueSendState* states, std::chrono::steady_clo
 static void do_send_messages() {
 #ifndef WIN32
     {
-        struct sched_param sched{sched_get_priority_max(SCHED_RR)};
+        struct sched_param sched = {};
+        sched.sched_priority = sched_get_priority_max(SCHED_RR);
         int res = pthread_setschedparam(pthread_self(), SCHED_RR, &sched);
         LogPrintf("UDP: %s write thread priority to SCHED_RR%s\n", !res ? "Set" : "Was unable to set", !res ? "" : (res == EPERM ? " (permission denied)" : " (other error)"));
         if (res) {
@@ -842,11 +869,11 @@ static void do_send_messages() {
                 FillChecksum(std::get<3>(msg), std::get<1>(msg), std::get<2>(msg));
 
                 if (send_state.local) {
-                    assert(std::get<2>(msg) == sizeof(UDPMessage) - 1); // UDPMessage is 1 byte larger than block messages
-
-                    if (write(udp_socks[group], &LOCAL_MAGIC_BYTES, sizeof(LOCAL_MAGIC_BYTES)) != sizeof(LOCAL_MAGIC_BYTES) ||
-                            write(udp_socks[group], &std::get<1>(msg), std::get<2>(msg)) != std::get<2>(msg)) {
-                        //TODO: Handle?
+                    assert(std::get<2>(msg) == sizeof(UDPMessage) - 1);
+                    int localSock = udp_socks.back();
+                    if (write(localSock, &LOCAL_MAGIC_BYTES, sizeof(LOCAL_MAGIC_BYTES)) != sizeof(LOCAL_MAGIC_BYTES) ||
+                        write(localSock, &std::get<1>(msg), std::get<2>(msg)) != std::get<2>(msg)) {
+                            // TODO: Handle write error (if needed)
                     }
                 } else {
 
@@ -867,8 +894,9 @@ static void do_send_messages() {
                         addrlen = sizeof(sockaddr_in);
                     }
 
-                    if (sendto(udp_socks[group], &std::get<1>(msg), std::get<2>(msg), 0, (sockaddr*)&ss, addrlen) != std::get<2>(msg)) {
-                        //TODO: Handle?
+                    int sendSock = std::get<0>(msg).IsIPv6() ? udp_socks[group * 2] : udp_socks[group * 2 + 1];
+                    if (sendto(sendSock, &std::get<1>(msg), std::get<2>(msg), 0, (sockaddr*)&ss, addrlen) != std::get<2>(msg)) {
+                        // TODO: Handle send error (e.g. log it)
                     }
                 }
 
@@ -1143,7 +1171,7 @@ void OpenUDPConnectionTo(const CService& addr, uint64_t local_magic, uint64_t re
     if (connection_type == UDP_CONNECTION_TYPE_INBOUND_ONLY)
         group = LOCAL_RECEIVE_GROUP;
 
-    OpenUDPConnectionTo(addr, {htole64(local_magic), htole64(remote_magic), group, fUltimatelyTrusted, connection_type});
+    OpenUDPConnectionTo(addr, {htole64_internal(local_magic), htole64_internal(remote_magic), group, fUltimatelyTrusted, connection_type});
 }
 
 void OpenPersistentUDPConnectionTo(const CService& addr, uint64_t local_magic, uint64_t remote_magic, bool fUltimatelyTrusted, UDPConnectionType connection_type, size_t group) {
@@ -1155,7 +1183,7 @@ void OpenPersistentUDPConnectionTo(const CService& addr, uint64_t local_magic, u
     if (mapPersistentNodes.count(addr))
         return;
 
-    UDPConnectionInfo info = {htole64(local_magic), htole64(remote_magic), group, fUltimatelyTrusted, connection_type};
+    UDPConnectionInfo info = {htole64_internal(local_magic), htole64_internal(remote_magic), group, fUltimatelyTrusted, connection_type};
     OpenUDPConnectionTo(addr, info);
     mapPersistentNodes[addr] = info;
 }

bitcoinfibre/bitcoinfibre

src/udpnet.cpp
@@ -80,7 +80,7 @@ static node::NodeContext* g_node_context; // Initialized by InitializeUDPConnect
 static CService LOCAL_WRITE_DEVICE_SERVICE(CNetAddr(), 1);
 static CService LOCAL_READ_DEVICE_SERVICE(CNetAddr(), 2);
 
-#define LOCAL_DEVICE_CHECKSUM_MAGIC htole64(0xdeadbeef)
+#define LOCAL_DEVICE_CHECKSUM_MAGIC htole64_internal(0xdeadbeef)
 
 // TODO: The checksum stuff is not endian-safe (esp the poly impl):
 static void FillChecksum(uint64_t magic, UDPMessage& msg, const unsigned int length)
@@ -228,26 +228,49 @@ bool InitializeUDPConnections(node::NodeContext* const node_context) {
 
     const std::vector<std::pair<unsigned short, uint64_t> > group_list(GetUDPInboundPorts());
     for (std::pair<unsigned short, uint64_t> port : group_list) {
-        udp_socks.push_back(socket(AF_INET6, SOCK_DGRAM, 0));
-        assert(udp_socks.back());
-
-        int opt = 1;
-        assert(setsockopt(udp_socks.back(), SOL_SOCKET, SO_REUSEADDR, &opt,  sizeof(opt)) == 0);
-        opt = 0;
-        assert(setsockopt(udp_socks.back(), IPPROTO_IPV6, IPV6_V6ONLY, &opt,  sizeof(opt)) == 0);
-        fcntl(udp_socks.back(), F_SETFL, fcntl(udp_socks.back(), F_GETFL) | O_NONBLOCK);
-
-        struct sockaddr_in6 wildcard;
-        memset(&wildcard, 0, sizeof(wildcard));
-        wildcard.sin6_family = AF_INET6;
-        memcpy(&wildcard.sin6_addr, &in6addr_any, sizeof(in6addr_any));
-        wildcard.sin6_port = htons(port.first);
-
-        if (bind(udp_socks.back(), (sockaddr*) &wildcard, sizeof(wildcard))) {
+        // Create separate IPv6 and IPv4 sockets for this UDP port
+        int sock6 = socket(AF_INET6, SOCK_DGRAM, 0);
+        if (sock6 < 0) {
             CloseSocketsAndReadEvents();
             return false;
         }
-        LogInfo("UDP: Bound to port %hd for group %lu with %lu Mbps\n", port.first, udp_socks.size() - 1, port.second);
+        int opt = 1;
+        assert(setsockopt(sock6, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == 0);
+        assert(setsockopt(sock6, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof(opt)) == 0);
+        fcntl(sock6, F_SETFL, fcntl(sock6, F_GETFL) | O_NONBLOCK);
+        struct sockaddr_in6 addr6;
+        memset(&addr6, 0, sizeof(addr6));
+        addr6.sin6_family = AF_INET6;
+        addr6.sin6_addr = in6addr_any;
+        addr6.sin6_port = htons(port.first);
+        if (bind(sock6, (sockaddr*)&addr6, sizeof(addr6)) != 0) {
+            CloseSocketsAndReadEvents();
+            close(sock6);
+            return false;
+        }
+        int sock4 = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sock4 < 0) {
+            CloseSocketsAndReadEvents();
+            close(sock6);
+            return false;
+        }
+        assert(setsockopt(sock4, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == 0);
+        fcntl(sock4, F_SETFL, fcntl(sock4, F_GETFL) | O_NONBLOCK);
+        struct sockaddr_in addr4;
+        memset(&addr4, 0, sizeof(addr4));
+        addr4.sin_family = AF_INET;
+        addr4.sin_addr.s_addr = INADDR_ANY;
+        addr4.sin_port = htons(port.first);
+        if (bind(sock4, (sockaddr*)&addr4, sizeof(addr4)) != 0) {
+            CloseSocketsAndReadEvents();
+            close(sock6);
+            close(sock4);
+            return false;
+        }
+        // Register both sockets and log the binding
+        udp_socks.push_back(sock6);
+        udp_socks.push_back(sock4);
+        LogInfo("UDP: Bound to port %hu for group %lu with %lu Mbps\n", port.first, (unsigned long)(udp_socks.size()/2 - 1), port.second);
     }
 
     event_base_read = event_base_new();
@@ -380,25 +403,29 @@ static void read_socket_func(evutil_socket_t fd, short event, void* arg) {
     std::chrono::steady_clock::time_point start(std::chrono::steady_clock::now());
 
     UDPMessage msg;
-    /* We will place the incoming UDP message payload into `msg`. However, not
-     * necessarily the incoming payload will fill the entire `UDPMessage`
-     * structure. Hence, zero-initialize `msg` here. */
-    struct sockaddr_in6 remoteaddr;
-    socklen_t remoteaddrlen = sizeof(remoteaddr);
+    sockaddr_storage remote_addr;
+    socklen_t remote_addr_len = sizeof(remote_addr);
+    ssize_t res = recvfrom(fd, &msg, sizeof(msg), MSG_DONTWAIT, (sockaddr*)&remote_addr, &remote_addr_len);
 
-    ssize_t res = recvfrom(fd, &msg, sizeof(msg), MSG_DONTWAIT, (sockaddr*)&remoteaddr, &remoteaddrlen);
     if (res < 0) {
         int err = errno;
         LogInfo("UDP: Error reading from socket: %d (%s)!\n", err, NetworkErrorString(err));
         return;
     }
-    assert(remoteaddrlen == sizeof(remoteaddr));
 
     if (size_t(res) < sizeof(UDPMessageHeader) || size_t(res) >= sizeof(UDPMessage))
         return;
 
+    CService remote_service;
+    if (((sockaddr*)&remote_addr)->sa_family == AF_INET6) {
+        remote_service = CService(*(sockaddr_in6*)&remote_addr);
+    } else if (((sockaddr*)&remote_addr)->sa_family == AF_INET) {
+        remote_service = CService(*(sockaddr_in*)&remote_addr);
+    } else {
+        return;
+    }
     std::unique_lock<std::recursive_mutex> lock(cs_mapUDPNodes);
-    std::map<CService, UDPConnectionState>::iterator it = mapUDPNodes.find(CService(remoteaddr));
+    auto it = mapUDPNodes.find(remote_service);
     if (it == mapUDPNodes.end())
         return;
     if (!CheckChecksum(it->second.connection.local_magic, msg, res))
@@ -416,7 +443,7 @@ static void read_socket_func(evutil_socket_t fd, short event, void* arg) {
             return;
         }
 
-        state.protocolVersion = le64toh(msg.msg.longint);
+        state.protocolVersion = le64toh_internal(msg.msg.longint);
         if (PROTOCOL_VERSION_MIN(state.protocolVersion) > PROTOCOL_VERSION_CUR(UDP_PROTOCOL_VERSION)) {
             LogInfo("UDP: Got min protocol version we didnt understand (%u:%u) from %s\n", PROTOCOL_VERSION_MIN(state.protocolVersion), PROTOCOL_VERSION_CUR(state.protocolVersion), it->first.ToStringAddrPort());
             send_and_disconnect(it);
@@ -471,7 +498,7 @@ static void read_socket_func(evutil_socket_t fd, short event, void* arg) {
             return;
         }
 
-        uint64_t nonce = le64toh(msg.msg.longint);
+        uint64_t nonce = le64toh_internal(msg.msg.longint);
         std::map<uint64_t, int64_t>::iterator nonceit = state.ping_times.find(nonce);
         if (nonceit == state.ping_times.end()) // Possibly duplicated packet
             LogInfo("UDP: Got PONG message without PING from %s\n", it->first.ToStringAddrPort());
@@ -634,7 +661,7 @@ static void timer_func(evutil_socket_t fd, short event, void* arg) {
 
         if (!(state.state & STATE_GOT_SYN_ACK) && origLastSendTime < now - 1000) {
             msg.header.msg_type = MSG_TYPE_SYN;
-            msg.msg.longint = htole64(UDP_PROTOCOL_VERSION);
+            msg.msg.longint = htole64_internal(UDP_PROTOCOL_VERSION);
             SendMessage(msg, sizeof(UDPMessageHeader) + 8, false, it);
             state.lastSendTime = now;
         }
@@ -648,7 +675,7 @@ static void timer_func(evutil_socket_t fd, short event, void* arg) {
         if ((state.state & STATE_INIT_COMPLETE) == STATE_INIT_COMPLETE && state.lastPingTime < now - 1000 * 60 * 15) {
             uint64_t pingnonce = FastRandomContext().rand64();
             msg.header.msg_type = MSG_TYPE_PING;
-            msg.msg.longint = htole64(pingnonce);
+            msg.msg.longint = htole64_internal(pingnonce);
             SendMessage(msg, sizeof(UDPMessageHeader) + 8, false, it);
             state.ping_times[pingnonce] = TicksSinceEpoch<std::chrono::microseconds>(SystemClock::now());
             state.lastPingTime = now;
@@ -775,7 +802,8 @@ static inline bool fill_cache(PerQueueSendState* states, std::chrono::steady_clo
 static void do_send_messages() {
 #ifndef WIN32
     {
-        struct sched_param sched{sched_get_priority_max(SCHED_RR)};
+        struct sched_param sched = {};
+        sched.sched_priority = sched_get_priority_max(SCHED_RR);
         int res = pthread_setschedparam(pthread_self(), SCHED_RR, &sched);
         LogInfo("UDP: %s write thread priority to SCHED_RR%s\n", !res ? "Set" : "Was unable to set", !res ? "" : (res == EPERM ? " (permission denied)" : " (other error)"));
         if (res) {
@@ -841,11 +869,11 @@ static void do_send_messages() {
                 FillChecksum(std::get<3>(msg), std::get<1>(msg), std::get<2>(msg));
 
                 if (send_state.local) {
-                    assert(std::get<2>(msg) == sizeof(UDPMessage) - 1); // UDPMessage is 1 byte larger than block messages
-
-                    if (write(udp_socks[group], &LOCAL_MAGIC_BYTES, sizeof(LOCAL_MAGIC_BYTES)) != sizeof(LOCAL_MAGIC_BYTES) ||
-                            write(udp_socks[group], &std::get<1>(msg), std::get<2>(msg)) != std::get<2>(msg)) {
-                        //TODO: Handle?
+                    assert(std::get<2>(msg) == sizeof(UDPMessage) - 1);
+                    int localSock = udp_socks.back();
+                    if (write(localSock, &LOCAL_MAGIC_BYTES, sizeof(LOCAL_MAGIC_BYTES)) != sizeof(LOCAL_MAGIC_BYTES) ||
+                        write(localSock, &std::get<1>(msg), std::get<2>(msg)) != std::get<2>(msg)) {
+                            // TODO: Handle write error (if needed)
                     }
                 } else {
 
@@ -866,8 +894,9 @@ static void do_send_messages() {
                         addrlen = sizeof(sockaddr_in);
                     }
 
-                    if (sendto(udp_socks[group], &std::get<1>(msg), std::get<2>(msg), 0, (sockaddr*)&ss, addrlen) != std::get<2>(msg)) {
-                        //TODO: Handle?
+                    int sendSock = std::get<0>(msg).IsIPv6() ? udp_socks[group * 2] : udp_socks[group * 2 + 1];
+                    if (sendto(sendSock, &std::get<1>(msg), std::get<2>(msg), 0, (sockaddr*)&ss, addrlen) != std::get<2>(msg)) {
+                        // TODO: Handle send error (e.g. log it)
                     }
                 }
 
@@ -1177,7 +1206,7 @@ void OpenUDPConnectionTo(const CService& addr, uint64_t local_magic, uint64_t re
     if (connection_type == UDP_CONNECTION_TYPE_INBOUND_ONLY)
         group = LOCAL_RECEIVE_GROUP;
 
-    OpenUDPConnectionTo(addr, {htole64(local_magic), htole64(remote_magic), group, fUltimatelyTrusted, connection_type});
+    OpenUDPConnectionTo(addr, {htole64_internal(local_magic), htole64_internal(remote_magic), group, fUltimatelyTrusted, connection_type});
 }
 
 void OpenPersistentUDPConnectionTo(const CService& addr, uint64_t local_magic, uint64_t remote_magic, bool fUltimatelyTrusted, UDPConnectionType connection_type, size_t group) {
@@ -1189,7 +1218,7 @@ void OpenPersistentUDPConnectionTo(const CService& addr, uint64_t local_magic, u
     if (mapPersistentNodes.count(addr))
         return;
 
-    UDPConnectionInfo info = {htole64(local_magic), htole64(remote_magic), group, fUltimatelyTrusted, connection_type};
+    UDPConnectionInfo info = {htole64_internal(local_magic), htole64_internal(remote_magic), group, fUltimatelyTrusted, connection_type};
     OpenUDPConnectionTo(addr, info);
     mapPersistentNodes[addr] = info;
 }